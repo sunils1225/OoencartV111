3f9ca8345bef6a03e3631a6b160929186878f37a
Sunilkumar <sunilkumarsj1994@gmail.com>
2026-02-23 10:11:57 +0530

    second commit

---
+++ src/test/java/PageObject/demopage.java
@@ -0,0 +1,11 @@
+package PageObject;
+
+import org.openqa.selenium.WebDriver;
+
+    public class demopage extends BasePage{
+	
+    public demopage (WebDriver driver) {
+    	super(driver);
+    }
+    
+}
---
+++ src/test/java/TestCases/TC008_demo.java
@@ -0,0 +1,20 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+import TestBase.BaseClass;
+
+public class TC008_demo extends BaseClass{
+
+	@Test(groups="master")
+	public void verify_demo() {
+	logger.info("***** Starting TC001_AccountRegistrationTest  ****");
+	logger.debug("This is a debug log message");
+	try{
+		
+	}catch(Exception e) {
+		
+	}
+		
+	}
+}
